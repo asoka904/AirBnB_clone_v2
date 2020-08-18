79f8f1aa845430f7cf5f7dcb62ef061a2ff4aee6
Jose Saldarriaga <jose.saldarriaga@outlook.com>
2020-08-17 23:58:09 -0500

    Added task 1

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,2 +1,20 @@
 #!/usr/bin/python3
+"""A Fabric script that generates a .tgz archive from the contents of the
+web_static folder of your AirBnB Clone repo, using the function do_pack.
+"""
 
+from fabric.api import run, local, sudo
+from datetime import datetime
+
+def do_pack():
+    '''Generates a .tgz archive from the contents
+    of the web_static'''
+
+    bak_file = 'versions/web_static_{:s}.tgz'\
+               .format(datetime.now().strftime("%Y%m%d%H%M%S"))
+    local('mkdir -p versions')
+    command = local("tar -cvzf " + bak_file + " ./web_static/")
+    if command.succeeded:
+        return bak_file
+    return None
+    
