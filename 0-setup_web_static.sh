0e21d173c599b3db16224ec9055fe9b353bd8da7
Jose Saldarriaga <jose.saldarriaga@outlook.com>
2020-08-17 23:56:50 -0500

    Added task 0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,3 +2,12 @@
 # Sets up your web servers for the deployment of web_static.
 
 
+sudo apt-get update
+sudo apt-get -y install nginx
+sudo ufw allow 'Nginx HTTP'
+sudo mkdir -p /data/web_static/{releases/test,shared}
+echo "Holberton School" | sudo tee /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown ubuntu:ubuntu -R /data
+sudo sed -i '/listen 80 default_server/a \\tlocation /hbnb_static/ {\n\t alias /data/web_static/current/;\n\t}' /etc/nginx/sites-available/default
+sudo service nginx restart
