5c58ef39d36105ca332b660480368f6ee89060db
Jose Saldarriaga <jose.saldarriaga@outlook.com>
2020-08-18 00:16:26 -0500

    Fix

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,46 +1,63 @@
-"""Fabric script that distributes an archive to your web servers:
-"""
-from fabric.api import *
-from os import path
+#!/usr/bin/python3
+from datetime import datetime
+from fabric.api import local, put, run, env
+import os.path
 
-env.hosts = ['52.200.131.229', '54.146.160.217']
+env.hosts = ['34.74.97.226', '18.209.18.155']
+env.user = "ubuntu"
+
+
+def do_pack():
+    """Function to pack the contents of web_static folder
+    """
+
+    now = datetime.now()
+    filename = "web_static_{}{}{}{}{}{}".format(now.year, now.month,
+                                                now.day, now.hour,
+                                                now.minute, now.second)
+    local("mkdir -p versions")
+
+    com = local(("tar -cvzf versions/{}.tgz web_static").format(filename))
+
+    if com.failed is True:
+        return None
+    else:
+        return ("versions/{}.tgz".format(filename))
 
 
 def do_deploy(archive_path):
     '''distributes an archive to your web servers'''
-    if not path.exists(archive_path):
+
+    if os.path.exists(archive_path) is False:
         return False
-    ret_value = True
-    d_folder = put(archive_path, '/tmp/')
-    if d_folder.failed:
-        ret_value = False
-    archive_file = archive_path.replace(".tgz", "").replace("versions/", "")
-    d_dest = run('mkdir -p /data/web_static/releases/' + archive_file + '/')
-    if d_dest.failed:
-        ret_value = False
-    d_unpack = run('tar -xzf /tmp/' + archive_file + '.tgz' +
-                   ' -C /data/web_static/releases/' + archive_file + '/')
-    if d_unpack.failed:
-        ret_value = False
-    d_cleanfile = run('rm /tmp/' + archive_file + '.tgz')
-    if d_cleanfile.failed:
-        ret_value = False
-    d_move = run('mv /data/web_static/releases/' + archive_file +
-                 '/web_static/* /data/web_static/releases/' + archive_file +
-                 '/')
-    if d_move.failed:
-        ret_value = False
-    d_cleanfolder = run('rm -rf /data/web_static/releases/' + archive_file +
-                        '/web_static')
-    if d_cleanfolder.failed:
-        ret_value = False
-    d_removeold = run('rm -rf /data/web_static/current')
-    if d_removeold.failed:
-        ret_value = False
-    d_createnew = run('ln -sf /data/web_static/releases/' + archive_file +
-                      '/' + ' /data/web_static/current')
-    if d_createnew.failed:
-        ret_value = False
-    if ret_value:
-        print("All tasks succeeded!")
-    return ret_value
+
+    file_non_ver = archive_path.replace("versions/", "")
+    file_non_ext = file_non_ver.replace(".tgz", "")
+    p1 = "/data/web_static/releases/"
+
+    try:
+        put(archive_path, "/tmp/")
+
+        run("mkdir -p /data/web_static/releases/{}/".
+            format(file_non_ext))
+
+        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
+            format(file_non_ver, file_non_ext))
+
+        run("rm /tmp/{}".format(file_non_ver))
+
+        run("mv {}{}/web_static/* {}{}/".
+            format(p1, file_non_ext, p1, file_non_ext))
+
+        run("rm -rf /data/web_static/releases/{}/web_static".
+            format(file_non_ext))
+
+        run("rm -rf /data/web_static/current")
+
+        run("ln -s /data/web_static/releases/{} /data/web_static/current".
+            format(file_non_ext))
+
+        return True
+
+    except:
+        return False
