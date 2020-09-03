088449393d8ed2049cc41f3ed89e7baa57202540
Jose Saldarriaga <jose.saldarriaga@outlook.com>
2020-09-02 23:59:32 -0500

    Added task 0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -10,5 +10,11 @@ def hello_world():
     return 'Hello HBNB!'
 
 
+@app.route('/hbnb', strict_slashes=False)
+def HBNB():
+    '''display “HBNB”'''
+    return "HBNB"
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
