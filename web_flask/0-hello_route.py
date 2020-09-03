f139c7c472288227bf55e9c2771661880881ffa7
Jose Saldarriaga <jose.saldarriaga@outlook.com>
2020-09-03 00:01:01 -0500

    Fixed order

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -10,11 +10,5 @@ def hello_world():
     return 'Hello HBNB!'
 
 
-@app.route('/hbnb', strict_slashes=False)
-def HBNB():
-    '''display “HBNB”'''
-    return "HBNB"
-
-
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
